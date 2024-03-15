4fb3fbd89da2e5962f5fcc6c9dd76b10ca952d08
Alast0r <eshenkove@gmail.com>
2024-03-16 01:47:05 +0500

    test

---
+++ test.py
@@ -0,0 +1,29 @@
+import time
+from plyer import notification
+from datetime import datetime
+import flet
+
+def main(page: flet.Page):
+    page.title = "Assistent"
+    page.theme_mode = "dark"
+    page.vertical_alignment = flet.MainAxisAlignment.CENTER
+
+    page.add(flet.Row([flet.IconButton(flet.icons.MAIL_LOCK_OUTLINED),]))
+
+# while True:
+#     now = datetime.now()
+#     formatted_time = now.strftime("%H:%M")
+
+#     def muing():
+#         notification.notify(
+#             title='Напоминание',
+#             message='Мьюинг',
+#             app_name='Assistent',
+#             # app_icon=r'C:\Users\Maksim\GIT\Assistant\sample.jpg',
+#         )
+
+#     muing()
+#     time.sleep(3600)
+    
+
+flet.app(target=main)
